7d6c091bbeda08a7f79394831ba328193159cda8
Anvesh <anveshmishra0508@gmail.com>
2024-07-24 05:25:13 +0530

    added standard tcp connection and accept and read loops for the client connection

--- pkg/services/vault.go
+++ pkg/services/vault.go
@@ -2,11 +2,15 @@ package services
 
 import (
 	// "fmt"
+	"fmt"
+	"net"
 )
 
 type Vault struct{
 	Host string
 	Port string
+	ln  net.Listener
+	quitch  chan struct{}
 }
 
 
@@ -14,5 +18,45 @@ func NewVault(config map[string]string) Vault{
 	return Vault{
 		Host: config["HOST"],
 		Port: config["PORT"],
+		quitch: make(chan struct{}, ULIMIT),
+	}
+}
+
+func (v *Vault) StartListening() error{
+	ln, err := net.Listen("tcp", v.Host+":"+v.Port)
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+	v.ln = ln
+	go v.acceptLoop()
+	<-v.quitch
+	return nil
+}
+
+func (v *Vault) acceptLoop() {
+	for{
+		conn, err := v.ln.Accept()
+		if err != nil{
+			fmt.Println("accept error:", err)
+			continue
+		}
+		go v.readLoop(conn)
+	}
+}
+
+func (v *Vault) readLoop(conn net.Conn){
+	defer conn.Close()
+	buf := make([]byte, 2048)
+
+	for{
+		n,err:= conn.Read(buf)
+		if err!=nil{
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg:=buf[:n]
+		fmt.Printf(string(msg))
 	}
 }
